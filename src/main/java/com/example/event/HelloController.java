0e34943e3b68235653a64140ae404f623aade65d
Raghad Thawabi <raghadmoh.tha@gmail.com>
2024-02-23 17:04:29 +0200

    hi

---
+++ src/main/java/com/example/event/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.event;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public int Calc(int a,int b){
+        return a+b;
+    }
+}
